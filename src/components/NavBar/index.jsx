eba63516a79c68692f51eafb55d217e367285ed9
cbullard0414 <carterlbullard2@gmail.com>
2025-02-23 20:13:26 -0600

    some minor changes, mostly pushing to pull from desktop work station.

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -5,21 +5,23 @@ import { useParams } from "react-router-dom";
 function NavBar(){
     return (
         <div id="navbar">
+
+            <h2 id='homebutton'>
+                Home
+            </h2>
+
             <Link to='/'>
             <h1 id='Title'>
                 Birthday Tracker
             </h1>
             </Link>
-
+            
             <Link to='/add'>
             <h2 id='addbutton'>
                 Add a Birthday
             </h2>
             </Link>
 
-            <h2 id='homebutton'>
-                Home
-            </h2>
         </div>
         
     )
