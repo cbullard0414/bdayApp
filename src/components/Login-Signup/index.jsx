cd59bbffd84d8e9126512e2df6e5cd8c61093923
cbullard0414 <carterlbullard2@gmail.com>
2025-02-27 16:06:22 -0600

    Finished rough draft of account page.

--- src/components/Login-Signup/index.jsx
+++ src/components/Login-Signup/index.jsx
@@ -13,8 +13,41 @@ function LoginSignup(){
             </div>
 
             <div id='Body'>
-                <h1>LoginBody</h1>
-                <h1>SignupBody</h1>
+                <div id='AccountBody'>
+                    <div id='UsernameFieldLogin'>
+                        <label htmlFor='username'id='usernameLabel'>Username</label> <br/>
+                        <input type='text' name='username' id='userfieldlogin' maxLength='12'/>
+                    </div>
+                    <div id='PasswordFieldLogin' >
+                        <label htmlFor='password'id='passwordLabel'>Password</label> <br/>
+                        <input type='password' name='password' id='passfieldlogin' maxLength='12'/>
+                    </div>
+
+                    <div id = 'loginsubmit'>
+                    <button id='loginbutton'>
+                        Submit
+                    </button>
+                    </div>
+
+                    <div id='UsernameFieldSignup'>
+                        <label htmlFor='username'id='usernameLabel'>Username</label> <br/>
+                        <input type='text' name='username' id='userfieldsignup' maxLength='12'/>
+                    </div>
+                    <div id='PasswordFieldSignup'>
+                        <label htmlFor='password'id='passwordLabel'>Password</label> <br/>
+                        <input type='password' name='password' id='passfieldsignup' maxLength='12'/>
+                    </div>
+                    <div id='ConfirmPassword'>
+                        <label htmlFor='confirm' id='confirmLabel'>Confirm Password</label><br/>
+                        <input type='password' name='confirm' id='passconfirmfield'/>
+                    </div>
+
+                    <div id = 'signupsubmit'>
+                    <button id='signupbutton'>
+                        Submit
+                    </button>
+                    </div>
+                </div>
             </div>
         </div>
         
